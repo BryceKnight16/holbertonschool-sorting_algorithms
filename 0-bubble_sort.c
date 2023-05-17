710bcff413c44bb54646d45a9b43da37278b4662
BryceKnight16 <6373@holbertonstudents.com>
2023-05-16 18:15:45 -0700

    function that sorts like a bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that uses bubble sort on an array of ints
+ * @array: the array
+ * @size: size of the array
+ * Return: Void
+ **/
+
+void bubble_sort(int *array, size_t size)
+{
+	int i;
+	int tmp;
+	int flag;
+
+	i = 0;
+
+	if (array == NULL)
+	{
+		return;
+	}
+
+	while (flag != INT_MAX)
+	{
+		tmp = INT_MAX;
+		while(i < size - 1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array [i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+			}
+			i = i + 1;
+			flag = tmp;
+		}
+		i = 0;
+	}
+}
