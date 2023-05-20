46a970b7649b299efcaaadf8e8fe1c5ad1ae952d
Kathryn6368 <6368@holbertonstudents.com>
2023-05-19 23:01:31 -0700

    fix head

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -66,39 +66,6 @@ void swap_node(listint_t **head, listint_t *a_node, listint_t *b_node)
 }
 
 /**
- * get_dnodeint_at_index - accesses given node number
- * @head: pointer to the double linked list
- * @index: the number of requested node
- * Return: addess of given node
- */
-listint_t *get_dnodeint_at_index(listint_t *head, unsigned int index)
-{
-	listint_t *ptr;
-	unsigned int counter;
-
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
-	ptr = head;
-	counter = 0;
-
-	while (counter < index)
-	{
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-
-		counter = counter + 1;
-		ptr = ptr->next;
-
-	}
-	return (ptr);
-}
-
-/**
  * listint_len - returns the number of elements of a list_t list
  *
  * @h: elements of the list
