7155d0f400aa33b5dd4a752879f2b646f8e6dc1a
Kathryn6368 <6368@holbertonstudents.com>
2023-05-20 21:31:43 -0700

    add function comments and other formatting chagens for betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,37 +1,62 @@
 #include "sort.h"
 
+/**
+ * swap - short description
+ * @i: a int *data type variable
+ * @j: a int *data type variable
+ *
+ * Return: type is void
+ */
 void swap(int *i, int *j)
 {
-    int tmp = *i;
-    *i = *j;
-    *j = tmp;
+	int tmp = *i;
+	*i = *j;
+	*j = tmp;
 }
 
+/**
+ * partition - short description
+ * @array: a int *data type variable
+ * @low: a int data type variable
+ * @high: a int data type variable
+ * @size: a size_t data type variable
+ *
+ * Return: type is int
+ */
 int partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low;
-    int j = low;
+	int pivot = array[high];
+	int i = low;
+	int j = low;
 
-    while (j < high) {
-        if (array[j] <= pivot) {
-            swap(&array[i], &array[j]);
-	    if (i != j)
-	    {
-		    print_array(array, size);
-	    }
-            i = i + 1;
-        }
-        j = j + 1;
-    }
-    swap(&array[i], &array[high]);
-    if (i != j)
+	while (j < high) {
+		if (array[j] <= pivot) {
+			swap(&array[i], &array[j]);
+			if (i != j)
+			{
+				print_array(array, size);
+			}
+			i = i + 1;
+		}
+		j = j + 1;
+	}
+	swap(&array[i], &array[high]);
+	if (i != j)
     {
 	    print_array(array, size);
     }
-    return i;
+	return i;
 }
 
+/**
+ * quicksort_recursive - short description
+ * @array: a int *data type variable
+ * @low: a int data type variable
+ * @high: a int data type variable
+ * @size: a size_t data type variable
+ *
+ * Return: type is void
+ */
 void quicksort_recursive(int *array, int low, int high, size_t size)
 {
 	int i;
